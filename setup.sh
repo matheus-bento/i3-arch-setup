5976c872e7664320b9e921fd834f8daeeb1faa28
Matheus Bento <matheus.bento@sinapseinformatica.com.br>
2021-02-12 12:40:10 -0300

    install commands corrections and echo formatting

--- setup.sh
+++ setup.sh
@@ -16,6 +16,8 @@ echo "\
                        |_|    \
 "
 
+sleep 0.8
+
 #region methods
 
 pacman_install() {
@@ -37,56 +39,72 @@ yay_install() {
 
 #region installations
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing i3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing i3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 pacman_install xorg sddm i3-gaps i3status dmenu picom virtualbox-guest-utils
 sudo systemctl enable sddm
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing konsole ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing konsole ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 pacman_install konsole
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing zsh ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing zsh ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 pacman_install zsh
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing oh-my-zsh ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing oh-my-zsh ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
+bash -c "RUNZSH='no';$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing yay ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-pacman_install git
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing yay ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 git clone https://aur.archlinux.org/yay ./temp
 
 cd ./temp
-
 makepkg -sir --noconfirm
 
 cd ..
 rm -rf ./temp
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing neovim ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing neovim ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 pacman_uninstall vim
 
 pacman_install neovim
 yay_install neovim-symlinks
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing neofetch ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing neofetch ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 pacman_install neofetch
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing picom ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing picom ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 pacman_install picom
+
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ installing feh ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
+pacman_install feh
 
 #endregion
 
-echo "~~~~~~~~~~~~~~~~~~~~ setting 1920x1080 resolution on xorg ~~~~~~~~~~~~~~~~~~~~~~"
+sudo localectl set-keymap br-abnt2
+sudo timedatectl set-timezone America/Sao_Paulo
+
+echo "\n\n~~~~~~~~~~~~~~~~~~~~ setting 1920x1080 resolution on xorg ~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 sudo cp ./xorg/10-monitor.conf /etc/X11/xorg.conf.d
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ applying dotfiles ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-rm ~/.zshrc
-rm ~/.zsh_history
-
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ applying dotfiles ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 cp -a ./dotfiles/. ~
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~ creating xdg user dirs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~ creating xdg user dirs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
 pacman_install xdg-user-dirs
 xdg-user-dirs-update
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ copying wallpaper ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cp ./resources ~/Pictures
+echo "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ copying wallpaper ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+sleep 0.8
+cp -r ./resources ~/Pictures
+
+echo "done!!!"
